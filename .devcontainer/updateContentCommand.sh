99604a5c56d25e305a9fb1ee126a4f732ca91bb7
sycod <julmat2@gmail.com>
2024-02-22 15:24:05 +0100

    Delete .devcontainer/updateContentCommand.sh

--- .devcontainer/updateContentCommand.sh
+++
@@ -1,4 +0,0 @@
-# devcontainer.json/updateContentCommand
-sudo apt update && sudo apt -y upgrade
-pip install --upgrade pip && pip install -r requirements.txt
-echo '✅ Packages installed and Requirements met'
