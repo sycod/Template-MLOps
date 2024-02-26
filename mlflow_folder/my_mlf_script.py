a7b790926c7657634023c7d675e1f4925632b01a
sycod <julmat2@gmail.com>
2024-02-26 13:20:38 +0100

    Update my_mlf_script.py

--- mlflow_folder/my_mlf_script.py
+++ mlflow_folder/my_mlf_script.py
@@ -2,7 +2,12 @@
 
 import mlflow
 
-def main():
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+from sklearn.ensemble import RandomForestRegressor
+
+
+def main_manual():
     client = mlflow.MlflowClient()
 
     mlflow.log_param("threshold", 4)
@@ -14,5 +19,18 @@ def main():
     mlflow.log_artifact("my_artifact.csv")
 
 
+def main_autolog():
+    mlflow.autolog()
+
+    db = load_diabetes()
+    X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
+
+    rf = RandomForestRegressor(n_estimators=100, max_depth=6, max_features=3)
+    # MLflow triggers logging automatically upon model fitting
+    rf.fit(X_train, y_train)
+
+
 if __name__ == '__main__':
-    main()
+    mlflow.start_run()
+    main_autolog()
+    mlflow.end_run()
