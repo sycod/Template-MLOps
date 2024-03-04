c39e3f64b3ac526a6393fb942d6c9bf3a3a9ddc8
sycod <julmat2@gmail.com>
2024-03-04 18:26:22 +0100

    Update patient.py

--- cli_example/patient.py
+++ cli_example/patient.py
@@ -1,3 +1,4 @@
+#/usr/bin/env python
 """A click test module."""
 
 import click
@@ -8,15 +9,15 @@ import click
 @click.option('--verbose', '-v', is_flag=True, help="Print more output.")
 def pid(patient_id, verbose):
     """Displays information from a patient ID."""
-    message = f"Patient ID {patient_id}, verbose {'on' if verbose else 'off'}"
-    return message
+    print(f"Patient ID {patient_id}, verbose {'on' if verbose else 'off'}")
 
 
-def another_function():
+@click.command()
+@click.argument('m')
+def another_function(m):
     """Another function's help, to test API."""
-
-    return "Another function's print."
+    print(f"Another function's print: {m}.")
 
 
 if __name__ == "__main__":
-    print(help(pid))
+    pid()
